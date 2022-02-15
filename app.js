e396873681a587f1eccafd5f2783d469d1ec150d
kim-kyoung <marycarychin@gmail.com>
2022-02-16 03:28:47 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const maxNum = document.querySelector("#maxNum");
+const guessNum = document.querySelector("#guessNum");
+const playBtn = document.querySelector("#playBtn");
+const result = document.querySelector("#result");
+const judge = document.querySelector("#judge");
+
+function playBtnOnclick(e) {
+  e.prevenetDefault();
+
+  result.classList.remove("hidden");
+  judge.classList.remove("hidden");
+  if (0 <= maxNum.value && guessNum.value <= maxNum.value) {
+    const machine = parseInt(Math.ceil(Math.random() * maxNum.value));
+    result.innerText = `your number is : ${guessNum.value},machine number is :${machine}`;
+    if (machine === maxNum.value) {
+      judge.innerText = "you win!";
+    } else {
+      judge.innerText = "you lost...";
+    }
+  }
+}
+
+playBtn.addEventListener("click", playBtnOnclick);
